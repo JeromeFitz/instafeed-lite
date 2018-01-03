98453c3bf64d0bfa63ff487cabb9b0aea2f37550
Jerome Fitzgerald <jeromes@gmail.com>
2018-01-03 09:17:04 -0500

    [fix] alter index.js to be function returning data

--- src/index.js
+++ src/index.js
@@ -1,4 +1,64 @@
+/* eslint-disable no-console */
 import 'babel-core/register'
 import 'babel-polyfill'
+import invariant from 'invariant'
 
-export { instafeed } from './modules'
+// import genKey from './genKey'
+import run from './modules/run'
+
+export default params => {
+  // console.log('index > params...')
+  // console.dir(params)
+  let option,
+    // unique,
+    value = null
+
+  // default options
+  const defaults = {
+    get: 'popular',
+    resolution: 'thumbnail',
+    sortBy: 'none'
+  }
+
+  invariant(
+    typeof params === 'object',
+    'You must pass an Object of options to the react-instafeed function'
+  )
+
+  // override default options
+  for (option in params) {
+    value = params[option]
+    defaults[option] = value
+  }
+
+  // console.log('index > defaults...')
+  // console.dir(defaults)
+
+  // unique = genKey()
+  // const data = run(defaults, unique, this)
+  const data = run(defaults, this)
+
+  // console.log('index > unique...')
+  // console.dir(unique)
+
+  // console.log('index > data...')
+  // console.dir(data)
+
+  return data
+  // return WrappedComponent => {
+  //   invariant(
+  //     typeof WrappedComponent === 'function' ||
+  //       typeof WrappedComponent === 'object',
+  //     `You must pass a component to the function returned by ` +
+  //       `index. Instead received ${JSON.stringify(WrappedComponent)}`
+  //   )
+  //
+  //   // eslint-disable-next-line react/display-name
+  //   return class extends Component {
+  //     render() {
+  //       console.log('... Component')
+  //       return <WrappedComponent {...data} />
+  //     }
+  //   }
+  // }
+}
